0d55309b88d6650f025c29c2f6575adf4073d086
Sufi-to <3thomas225@gmail.com>
2024-01-03 23:10:56 +0400

    fizzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if (i == 100):
             print("{}".format("Buzz"))
-        if i % 3 == 0 and i % 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
