eb97c9c03773b819a2ceb7b6199942cd44db81d5
Sufi-to <3thomas225@gmail.com>
2024-01-03 22:45:44 +0400

    function tasks

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strupp = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 123:
+            strupp += chr(ord(i) - 32)
+        else:
+            strupp += i
+
+    print(strupp)
