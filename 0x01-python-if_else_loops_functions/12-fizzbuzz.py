699e7960f1fdb16932ed4c9dbf18c1c82c6e2743
Sufi-to <3thomas225@gmail.com>
2024-01-03 22:53:08 +0400

    fixed minor errors

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i == 100:
-            print("Buzz")
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
@@ -10,6 +8,8 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(i, end=" ")
-
-fizzbuzz()
+            if i == 100:
+                break
+            else:
+                print(i, end=" ")
+    print()
