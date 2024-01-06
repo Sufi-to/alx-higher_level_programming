05ba1e960f9718dcc1de83149569301f3b56b466
Sufi-to <3thomas225@gmail.com>
2024-01-06 19:06:37 +0400

    return largest

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        largest = 0
+        largest = my_list[0]
         for i in my_list:
             if i > largest:
                 largest = i
