0704eb62ee4755595ed7cfd1378b5cbe9b307bb9
Sufi-to <3thomas225@gmail.com>
2024-01-11 05:44:00 +0400

    roman number to digit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,16 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+
     if not isinstance(roman_string, str):
         return 0
 
     roman_lit = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
                  "D": 500, "M": 1000}
     num_ls = [roman_lit[i] for i in roman_string]
-    num = 0
     if len(num_ls) == 1:
         return num_ls[0]
+    num = 0
     for x in range(len(num_ls)-1):
         if num_ls[x] >= num_ls[x + 1]:
             num += num_ls[x]
