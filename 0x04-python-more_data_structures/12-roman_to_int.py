d9f6afe3c69a5e003a5170005129282a4eb77987
Sufi-to <3thomas225@gmail.com>
2024-01-11 05:40:37 +0400

    roman number to int final

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,23 +2,23 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    if isinstance(roman_string, str) and roman_string is not None:
-        roman_lit = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-                     "D": 500, "M": 1000}
-        num_ls = [roman_lit[i] for i in roman_string]
-        num = 0
-        if len(num_ls) == 1:
-            return num_ls[0]
-        for x in range(len(num_ls)-1):
-            if num_ls[x] >= num_ls[x + 1]:
-                num += num_ls[x]
-            else:
-                num -= num_ls[x]
-        if num_ls[-1] >= num_ls[-2]:
-            num += num_ls[-1]
-        else:
-            num -= num_ls[-1]
-    else:
+    if not isinstance(roman_string, str):
         return 0
+
+    roman_lit = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
+                 "D": 500, "M": 1000}
+    num_ls = [roman_lit[i] for i in roman_string]
+    num = 0
+    if len(num_ls) == 1:
+        return num_ls[0]
+    for x in range(len(num_ls)-1):
+        if num_ls[x] >= num_ls[x + 1]:
+            num += num_ls[x]
+        else:
+            num -= num_ls[x]
+    if num_ls[-1] >= num_ls[-2]:
+        num += num_ls[-1]
+    else:
+        num -= num_ls[-1]
 
     return num
