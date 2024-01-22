9fe0bf77caaa5922f9ee828f308086faf42339b7
Sufi-to <3thomas225@gmail.com>
2024-01-22 16:50:30 +0400

    try except error handling

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+	try:
+		ln = 0
+		for i in range(x):
+			print(my_list[i], end="")
+			ln += 1
+		print()
+	except IndexError:
+		print()
+
+	return ln
+
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+	x = []
+	for i in range(list_length):
+		try:
+			x.append(my_list_1[i] / my_list_2[i])
+		except(ZeroDivisionError):
+			print("division by zero")
+			x.append(0)
+		except(TypeError):
+			print("wrong type")
+			x.append(0)
+		except(IndexError):
+			print("out of range")
+			x.append(0)
+		finally:
+			continue
+	return x
