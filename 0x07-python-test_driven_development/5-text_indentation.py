036588f5db3875f33b418236fece3cf304faa38d
Sufi-to <3thomas225@gmail.com>
2024-01-31 19:23:01 +0400

    text indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,3 +6,14 @@ def text_indentation(text):
     """Indent text wheneven you find the symbols."""
     if type(text) is not str:
         raise TypeError("text must be a string")
+
+    spl = [":", "?", "."]
+    new_str = ""
+    for i in text:
+        if i in spl:
+            new_str += i
+            print(new_str)
+            print()
+            new_str = ""
+        else:
+            new_str += i
