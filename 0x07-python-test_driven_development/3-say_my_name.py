1fe04aa84eb276f8bcbec41f181dfb4b7329f907
Sufi-to <3thomas225@gmail.com>
2024-01-31 18:50:58 +0400

    check string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """Print the a message with the first and last names."""
-    if type(first_name) is type(""):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) is type(""):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
