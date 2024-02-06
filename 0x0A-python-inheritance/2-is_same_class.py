b1a55354e8f93e4955b11d94b3dc63b1f63e091c
Sufi-to <3thomas225@gmail.com>
2024-02-06 07:01:28 +0400

    is type of

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
+"""Contains the is_same_class function."""
+
+
 def is_same_class(obj, a_class):
+    """Return true if obj is type of a_class."""
     return (True if type(obj) is a_class else False)
 
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
