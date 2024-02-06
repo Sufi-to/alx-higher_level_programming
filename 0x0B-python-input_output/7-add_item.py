1433673f3c8094e1abc8e6fc6d748242417e25d2
Sufi-to <3thomas225@gmail.com>
2024-02-06 13:35:12 +0400

    add_items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,15 +9,12 @@ from pathlib import Path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-if __name__ == "__main__":
-    """Run this file only when __name__ = main."""
-    data_from_json_file = []
-    filename = "add_item.json"
-    if len(argv) == 1 and (not Path(filename).exists()):
-        save_to_json_file(data_from_json_file, filename)
-    if len(argv) > 1:
-        data_from_json_file = load_from_json_file(filename)
-        for i in range(1, len(argv)):
-            data_from_json_file.append(argv[i])
-        save_to_json_file(data_from_json_file, filename)
+data_from_json_file = []
+filename = "add_item.json"
+if len(argv) == 1 and (not Path(filename).exists()):
+    save_to_json_file(data_from_json_file, filename)
+if len(argv) > 1:
+    data_from_json_file = load_from_json_file(filename)
+    for i in range(1, len(argv)):
+        data_from_json_file.append(argv[i])
+    save_to_json_file(data_from_json_file, filename)
