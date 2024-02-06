10035d480b2cd99393c1cdf9eb6f5464f116a49e
Sufi-to <3thomas225@gmail.com>
2024-02-06 07:02:42 +0400

    is type of

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Contains the look_up function."""
+
+
 def lookup(obj):
+    """Return what methods and attributes does obj have."""
     if obj:
         return (dir(obj))
-
-
-print(lookup(list))
