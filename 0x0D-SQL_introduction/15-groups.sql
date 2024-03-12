c6a8a30eafe5f4dc49ecadb580ecad49caec8626
Sufi-to <3thomas225@gmail.com>
2024-03-12 20:56:29 +0400

    grouped and ordered

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- list the number of records with the same score
 -- in the table second_table or the database
-SELECT score, COUNT(*) as number
+SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY number;
+GROUP BY number
+ORDER BY number DESC;
