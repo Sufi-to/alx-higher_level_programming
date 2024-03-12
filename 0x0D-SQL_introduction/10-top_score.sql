caa139f8d3b8f2f8d80837d5145398f155e22f5e
Sufi-to <3thomas225@gmail.com>
2024-03-12 20:52:30 +0400

    top first

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- list all the records in the table second_table
 -- of the database hbtn_0c_0 in mysql server
-SELECT score, name FROM second_table ORDER_BY score DESC;
+SELECT score, name
+FROM second_table
+ORDER_BY score DESC;
