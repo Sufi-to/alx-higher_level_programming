d3a94a1a41ac2b4fea4d230b6af4874313053a22
Sufi-to <3thomas225@gmail.com>
2024-03-14 14:29:00 +0400

    done

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,6 @@
 -- import in hbtn_0c_0 to diplay the average temp(F)
 -- by city ordered by temp(desc)
-
+SELECT city, AVG(value) AS avg_temp
+FROM `temperatures`
+GROUP BY city
+ORDER BY avg_temp DESC;
