d891a82ae420cbec16e26944f75a6a2556766551
Sufi-to <3thomas225@gmail.com>
2024-04-08 22:05:33 +0400

    good job

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 
 const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i=0; i < myArray.length; i++) {
-	console.log(myArray[i]);
+  console.log(myArray[i]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 
 const myVar = "C is fun";
 const no = parseInt(process.argv[2]);
-
 if (process.argv.length === 2) {
-	console.log('Missing number of occurrences');
+    console.log('Missing number of occurrences');
 } else if (no) {
-	for (let i=1; i <= no; i++) {
-		console.log(myVar);
-	}
+    for (let i=1; i <= no; i++) {
+        console.log(myVar);
+    }
 }
