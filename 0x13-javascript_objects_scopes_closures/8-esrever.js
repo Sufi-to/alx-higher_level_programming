71ad94c41a86ba1953f8f06e109050877fde3713
Tomas Teclehaimanot <80114437+Sufi-to@users.noreply.github.com>
2024-04-09 21:16:11 +0000

    let's go

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-
-	let arr = [];
-	if (list) {
-		for (let i = list.length - 1; i >= 0; i--) {
-			arr.push(list[i]);
-		}
-	}
-	return arr;
-}
+  const arr = [];
+  if (list) {
+    for (let i = list.length - 1; i >= 0; i--) {
+      arr.push(list[i]);
+    }
+  }
+  return arr;
+};
