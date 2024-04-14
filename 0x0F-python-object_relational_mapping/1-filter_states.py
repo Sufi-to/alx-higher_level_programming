5d73d49ba3537156663e6fa93082b884aca390b3
Sufi-to <3thomas225@gmail.com>
2024-04-14 18:18:40 +0400

    please work

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" list all the states that are like a given name from the database """
+"""lists all the states that are like a given name from the database"""
 import MySQLdb
 import sys
 
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     match = "N%"
-    cur.execute("""SELECT * FROM states WHERE name
+    cur.execute("""SELECT * FROM states WHERE BINARY name
                 LIKE %s ORDER BY id""", (match, ))
     query_rows = cur.fetchall()
     for row in query_rows:
