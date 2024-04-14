2ec70d7a6d97c1d488750576e66ec084e24f0158
Sufi-to <3thomas225@gmail.com>
2024-04-14 17:39:08 +0400

    it is what it is

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2],
-                        db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '{}'"
                 .format(sys.argv[4]))
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,7 +10,9 @@ from sqlalchemy.ext.declarative import declarative_base
 this_metadata = MetaData()
 Base = declarative_base(metadata=this_metadata)
 
+
 class State(Base):
+    """ This is a state class """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
-    name =  Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
