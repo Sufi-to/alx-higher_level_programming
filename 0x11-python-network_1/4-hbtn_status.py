f72a6f8f1a43a65066cfe412a0a4597c90409774
Sufi-to <3thomas225@gmail.com>
2024-04-27 03:56:27 +0400

    fixed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.reason}")
+    print(f"\t- content: {r.text}")
