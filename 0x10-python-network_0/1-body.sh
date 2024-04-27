930a32da29530303ff312babfdd359f651ea0195
Sufi-to <3thomas225@gmail.com>
2024-04-27 22:34:44 +0400

    curling my life away

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that sends a get request and display the body
 # if the status code is 200
-curl -sX GET -L "$1"
+curl -s -X GET -L "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes all HTTP methods the server will accept
-curl -SI "$1" | grep -i allow | sed -ne 's/^Allow: //p'
+curl -sX OPTIONS -I "$1"
