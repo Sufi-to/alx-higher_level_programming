b9dc5f14055250c42e599b149e50fc6060e762f8
Sufi-to <3thomas225@gmail.com>
2024-04-28 01:13:01 +0400

    hell

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # script that takes all HTTP methods the server will accept
-response=$(curl -s -X OPTIONS -i "$1")
-allow_opts=$(echo "$response" | grep -i "Allow")
-if [ -n "$allow_opts" ];
-then
-	methods=$(echo "$allow_opts" | awk '{print substr($0, 8)}')
-	echo "$methods"
-fi
+curl -s -X OPTIONS -I "$1"
