a4a25d600fbc86cf34802bfc09e2d71ef98dd197
Sufi-to <3thomas225@gmail.com>
2024-04-28 01:23:44 +0400

    print a response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # script that sends a get request and display the body
 # if the status code is 200
-curl -s -X GET -L "$1"
+response=$(curl -s -w "%{http_code}" -L "$1")
+stat_code=$(echo "$response" | tail -c 4)
+if [ "$stat_code" = "200" ]
+then
+	echo "$response" | head -c 4
+fi
