01cf74aec734dd11831d0cf8ea93a9d491a7fc70
Sufi-to <3thomas225@gmail.com>
2024-04-27 22:15:07 +0400

    curling this thing

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that sends a get request and display the body
 # if the status code is 200
-curl -sL "$1"
+curl -sX GET -L "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a delete request and diplay the body of response
-curl -X DELETE "$1"
+curl -sX DELETE "$1"
