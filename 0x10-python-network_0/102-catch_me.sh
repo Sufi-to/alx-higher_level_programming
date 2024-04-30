3a969d419483063d2c42416844bdb3617bfac40a
Sufi-to <3thomas225@gmail.com>
2024-04-30 22:55:06 +0400

    flags changed

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that makes a request to a given url and
 # causes the server to response "you got me"
-curl -sX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
