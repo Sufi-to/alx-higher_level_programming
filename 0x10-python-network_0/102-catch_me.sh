a29c5c5a82a4752e59cac884734a528933271f20
Sufi-to <3thomas225@gmail.com>
2024-04-30 22:51:23 +0400

    curl put

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # script that makes a request to a given url and
 # causes the server to response "you got me"
+curl -sX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
