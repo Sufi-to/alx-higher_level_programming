b6e9c91ca72dd9537614f82ba98e5d40158dc9b0
Sufi-to <3thomas225@gmail.com>
2024-04-30 22:46:55 +0400

    silent the error

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends json Post request and display body response
-curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
