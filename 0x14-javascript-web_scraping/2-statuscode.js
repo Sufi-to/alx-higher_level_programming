0525c8ccdc9e334426f3cecc5680e62f101297a8
Sufi-to <3thomas225@gmail.com>
2024-05-21 20:01:27 +0400

    status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  console.error('error:', error);
+request(process.argv[2], function (response) {
   console.log('code: ', response && response.statusCode);
 });
