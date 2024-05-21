49d696da7211f80246a9c59deafc096d6eb192bb
Sufi-to <3thomas225@gmail.com>
2024-05-21 19:49:27 +0400

    write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const content = process.argv[3]
+const content = process.argv[3];
 fs.writeFile(process.argv[2], content, err => {
 	if (err) {
 		console.log(err);
